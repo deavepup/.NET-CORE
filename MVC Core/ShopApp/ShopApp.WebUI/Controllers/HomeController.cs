956ab549406666aec05b7ec4f61c3e9012bbe0a3
Hüseyin Dönmez <deavepup@gmail.com>
2022-01-28 17:23:08 +0300

    App

---
+++ MVC Core/ShopApp/ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+              return View();
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+        public IActionResult Contact()
+        {
+
+            return View("MyView");
+        }
+    }
+}
---
+++ MVC Core/ShopApp/ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List(int id)
+        {
+
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+
+            return View();
+        }
+    }
+}
