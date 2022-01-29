30f2e6bba33cea9d8f20938b2a98af2ead755476
Hüseyin Dönmez <deavepup@gmail.com>
2022-01-29 21:04:05 +0300

    App

---
+++ MVC Core/ShopApp/ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.WebUI.Models;
+using ShopApp.WebUI.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Name ="Iphone 7",Price =3000,Description="İyi Telefon",IsApproved=false},
+                new Product { Name ="Iphone 8",Price =4000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone X",Price =5000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone 11",Price =7000,Description="Çok iyi Telefon"}
+            };
+
+            var productViewModel = new ProductViewModel()
+            {
+                Products = products
+            };
+            return View(productViewModel);
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+        public IActionResult Contact()
+        {
+
+            return View("MyView");
+        }
+    }
+}
---
+++ MVC Core/ShopApp/ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.WebUI.Models;
+using ShopApp.WebUI.ViewModels;
+using System.Collections.Generic;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Name ="Iphone 7",Price =3000,Description="İyi Telefon",IsApproved=false},
+                new Product { Name ="Iphone 8",Price =4000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone X",Price =5000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone 11",Price =7000,Description="Çok iyi Telefon"}
+            };
+
+            var productViewModel = new ProductViewModel()
+            {
+                Products = products
+            };
+            return View(productViewModel);
+        }
+        public IActionResult List()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Name ="Iphone 7",Price =3000,Description="İyi Telefon",IsApproved=false},
+                new Product { Name ="Iphone 8",Price =4000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone X",Price =5000,Description="Çok iyi Telefon",IsApproved=true},
+                new Product { Name ="Iphone 11",Price =7000,Description="Çok iyi Telefon"}
+            };
+
+            var productViewModel = new ProductViewModel()
+            {
+                Products = products
+            };
+            return View(productViewModel);
+        }
+        public IActionResult Details(int id)
+        {
+            // name: "samsung s6"
+            // price: 3000
+            // description: "iyi telefon"
+
+            // ViewBag.Name = "samsung s6";
+            // ViewBag.Price = 3000;
+            // ViewBag.Description = "iyi telefon";
+
+            var p = new Product();
+
+            p.Name = "Samsung S6";
+            p.Price = 3000;
+            p.Description = "İyi Telefon";
+
+            return View(p);
+        }
+    }
+}
